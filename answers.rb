98576fcd2f43b363518debcb48059cd21659e584
Emilio <em.francomartinez@gmail.com>
2018-11-08 18:06:06 -0700

    updated main.rb and answers.rb

--- answers.rb
+++ answers.rb
@@ -1,6 +1,8 @@
 require "pry"
 
 class Answer
+  attr_accessor :answers 
+
   def initialize
     @answers = [
       "You win!",
@@ -12,5 +14,26 @@ class Answer
       "It's a great day!",
       "This is your year!",
     ]
+    binding.pry
   end
+
+  # def new_answer
+  #   answer_clone = @answers.clone
+  #   puts "*******MENU********"
+  #   puts "1) View answer list\n2) Add new answer\n3) Reset Answer"
+  #   user_choice = gets.strip.to_i
+    
+  #   case user_choice
+  #   when 1
+  #     binding.pry
+  #     puts @answers
+  #   when 2
+  #     puts "Enter new answer:"
+  #     new_string = gets.strip
+  #     @answers << new_string
+  #     binding.pry
+  #   when 3
+  #     @answers = answer_clone
+  #   end
+  # end
 end
--- main.rb
+++ main.rb
@@ -1,11 +1,13 @@
 require_relative "./answers"
 
+require "pry"
 class EightBall < Answer
   attr_accessor :name, :question
 
   def initialize(name, question)
     @name = name
     @question = question
+    @answer_clone = []
     super()
     create_answer
   end
@@ -13,6 +15,36 @@ class EightBall < Answer
   def create_answer
     answer = @answers.sample
     puts "#{name}, you asked: #{question}. The answer is: #{answer}"
+  end
+end
+
+def menu
+  puts "*******WELCOME TO EIGHT BALL********"
+  puts "1) Ask eight ball question\n2) Game menu"
+  menu_ans =gets.strip.to_i
+  if menu_ans == 1
+    get_user_input
+  else
+    new_answer
+    menu
+  end
+end
+
+def new_answer
+  @answer_clone = @answers.clone
+  puts "*******MENU********"
+  puts "1) View answer list\n2) Add new answer\n3) Reset Answer"
+  user_choice = gets.strip.to_i
+  
+  case user_choice
+  when 1
+    puts @answers
+  when 2
+    puts "Enter new answer:"
+    new_string = gets.strip
+    @answers << new_string
+  when 3
+    @answers = @answer_clone
   end
 end
 
@@ -25,4 +57,4 @@ def get_user_input
   EightBall.new(name, question)
 end
 
-get_user_input
+menu
