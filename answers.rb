52273bdc6d2933f824acd22c050db9e28c4eb816
Kevin Peery <kevin.peery@gmail.com>
2018-11-09 07:28:55 -0700

    add methods to EightBall class

--- answers.rb
+++ answers.rb
@@ -14,3 +14,12 @@ class Answer
     ]
   end
 end
+
+#approach 1
+#could have a separate array that any new response goes into...when they add. When they reset it empties it. You need other logic to bring it in and add to all the answers.
+
+#maybe make a new method or store the variable as an instance variable (@) in a class.
+
+#.clone take the existing array and call .clone on that array and set it equal to a new array, maybe @new_answers or @user_answers.
+
+#to do this, create a new method and call it in the answer_menu method.
